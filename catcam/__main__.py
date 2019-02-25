f60857bedcf0100c59fb45e8429acc28dae4e097
Jenny <jenny.thorne3006@gmail.com>
2019-02-25 21:33:31 +0000

    pushy

--- catcam/__main__.py
+++ catcam/__main__.py
@@ -5,12 +5,8 @@ from picamera import PiCamera
 import time
 from catcam import settings
 import requests
-<<<<<<< HEAD
-from requests import HTTPError
+from requests import ResponseError
 
-=======
-from requests.exceptions import RequestException
->>>>>>> 3202aa048f2dba7d6ecf679475b011c13f22b717
 
 logger = logging.getLogger(__name__)
 
@@ -30,33 +26,19 @@ def main():
             camera.hflip = True
             time.sleep(2)
             camera.capture(stream, format="jpeg")
-<<<<<<< HEAD
-=======
-
->>>>>>> 3202aa048f2dba7d6ecf679475b011c13f22b717
             logger.info("Picture taken! Snap!")
             # Send the picture.
             logger.info("Sending pic...")
             try:
-<<<<<<< HEAD
                 url = "{}/node_manager/{}".format(settings.HUB_URL, settings.NODE_ID)
-                response = requests.post(url, stream,)
+                files = {'image': stream}
+                response = requests.post(url, files,)
                 response.raise_for_status()
 
-            except HTTPError as e:
-                status_code = e.response.status_code
-                logger.warning("Could not send pic: {}".format(status_code))
+            except ResponseError as e:
+                logger.warning("Could not send pic: {}".format(e))
 
             logger.info("Pic sent!")
-=======
-                url = "{}node_manager/{}/".format(settings.HUB_URL, settings.NODE_ID)
-                files = {'image': stream}
-                requests.post(url, files,)
-            except RequestException as ex:
-                logger.warning("Could not send pic: %s", ex)
-            else:
-                logger.info("Pic sent!")
->>>>>>> 3202aa048f2dba7d6ecf679475b011c13f22b717
         # All done!
         logger.info("Waiting for a bit...")
         time.sleep(10)
