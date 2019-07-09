fd13538da4def8db7afb108cbc162c73337f527b
Jenny <jenny.thorne3006@gmail.com>
2019-07-09 17:50:02 +0100

    pushy

--- catcam/__main__.py
+++ catcam/__main__.py
@@ -6,7 +6,7 @@ import time
 from catcam import settings
 import requests
 from requests.exceptions import RequestException
-
+from signal import pause
 
 logger = logging.getLogger(__name__)
 
@@ -14,11 +14,8 @@ logger = logging.getLogger(__name__)
 def main():
     logging.basicConfig(format="[%(levelname)s %(name)s] %(message)s", level=logging.INFO)
     pir = MotionSensor(4)
-    logger.info("CatCam has started")
-    while True:
-        # Wait for motion.
-        logger.info("Waiting for motion...")
-        pir.wait_for_motion()
+
+    def motion_detected():
         logger.info("Motion detected! It's a cat!")
         # Take a picture.
         with PiCamera() as camera, BytesIO() as stream:
@@ -43,6 +40,11 @@ def main():
         # All done!
         logger.info("Waiting for a bit...")
         time.sleep(60*30)
+
+    pir.when_motion = motion_detected
+
+    logger.info("CatCam has started")
+    pause()
 
 
 if __name__ == "__main__":
