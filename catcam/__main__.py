d157fb0a457a63652260df6db50cd7f5d458127f
Jenny <jenny.thorne3006@gmail.com>
2019-07-09 18:16:45 +0100

    pushy

--- catcam/__main__.py
+++ catcam/__main__.py
@@ -39,7 +39,7 @@ def main():
             logger.info("Pic sent!")
         # All done!
         logger.info("Waiting for a bit...")
-        time.sleep(30)
+        time.sleep(60*30)
 
     pir.when_motion = motion_detected
 
