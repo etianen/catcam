11cd1ad25f065dfd28b2fc98ed48254d0eabec4e
Jenny <jenny.thorne3006@gmail.com>
2019-07-09 22:14:09 +0100

    pushy

--- catcam/__main__.py
+++ catcam/__main__.py
@@ -3,6 +3,7 @@ from io import BytesIO
 import logging
 from picamera import PiCamera
 import time
+from time import datetime
 from catcam import settings
 import requests
 from requests.exceptions import RequestException
@@ -14,32 +15,35 @@ logger = logging.getLogger(__name__)
 def main():
     logging.basicConfig(format="[%(levelname)s %(name)s] %(message)s", level=logging.INFO)
     pir = MotionSensor(4)
+    time_stamp = datetime.datetime.now
+    delay = datetime.timedelta(minutes=5)
 
     def motion_detected():
-        logger.info("Motion detected! It's a cat!")
-        # Take a picture.
-        with PiCamera() as camera, BytesIO() as stream:
-            camera.vflip = True
-            camera.hflip = True
-            time.sleep(1)
-            camera.capture(stream, format="jpeg")
-            stream.seek(0)
-            logger.info("Picture taken! Snap!")
-            # Send the picture.
-            logger.info("Sending pic...")
-            try:
-                url = "{}/node_manager/{}/".format(settings.HUB_URL, settings.NODE_ID)
-                files = {'image': ("image.jpeg", stream, "image/jpeg")}
-                response = requests.post(url, files=files,)
-                response.raise_for_status()
+        global time_stamp
+        if datetime.datetime.now > (time_stamp + delay):
+            logger.info("Motion detected! It's a cat!")
+            # Take a picture.
+            with PiCamera() as camera, BytesIO() as stream:
+                camera.vflip = True
+                camera.hflip = True
+                time.sleep(1)
+                camera.capture(stream, format="jpeg")
+                stream.seek(0)
+                logger.info("Picture taken! Snap!")
+                # Send the picture.
+                logger.info("Sending pic...")
+                try:
+                    url = "{}/node_manager/{}/".format(settings.HUB_URL, settings.NODE_ID)
+                    files = {'image': ("image.jpeg", stream, "image/jpeg")}
+                    response = requests.post(url, files=files,)
+                    response.raise_for_status()
 
-            except RequestException as e:
-                logger.warning("Could not send pic: {}".format(e))
+                except RequestException as e:
+                    logger.warning("Could not send pic: {}".format(e))
 
-            logger.info("Pic sent!")
-        # All done!
-        logger.info("Waiting for a bit...")
-        time.sleep(60*30)
+                logger.info("Pic sent!")
+            time_stamp = datetime.datetime.now
+            # All done!
 
     pir.when_motion = motion_detected
 
