0854a72f9c16d6d5b1db3d712d8785f91484ed32
Jenny <jenny.thorne3006@gmail.com>
2020-01-12 19:52:58 +0000

    pushy

--- catcam/__main__.py
+++ catcam/__main__.py
@@ -1,13 +1,10 @@
 from gpiozero import MotionSensor
-from io import BytesIO
 import logging
 from picamera import PiCamera
 import time
 from datetime import timedelta
 from datetime import datetime
 from catcam import settings
-import requests
-from requests.exceptions import RequestException
 from signal import pause
 
 logger = logging.getLogger(__name__)
@@ -16,45 +13,22 @@ logger = logging.getLogger(__name__)
 def main():
     logging.basicConfig(format="[%(levelname)s %(name)s] %(message)s", level=logging.INFO)
     pir = MotionSensor(17)
-    delay = timedelta(minutes=5)
+    delay = timedelta(minutes=1)
     time_stamp = datetime.now() - delay
 
     def motion_detected():
         nonlocal time_stamp
         if datetime.now() > (time_stamp + delay):
             logger.info("Motion detected!")
-            # Take a picture.
-            with PiCamera() as camera, BytesIO() as stream:
-                camera.vflip = True
-                camera.hflip = True
-                camera.rotation = 270
-                time.sleep(1)
-                camera.capture(stream, format="jpeg")
-                stream.seek(0)
-                logger.info("Picture taken! Snap!")
-                # Record a video and save to a file
-                logger.info("Starting video...")
-                camera.resolution = (640, 480)
-                file_name = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-                camera.start_recording('/home/pi/videos/{}.h264'.format(file_name))
-                camera.wait_recording(180)
-                camera.stop_recording()
-                logger.info("Saved video!")
-                # Send the picture.
-                logger.info("Sending pic...")
-                try:
-                    url = "{}/node_manager/{}/".format(settings.HUB_URL, settings.NODE_ID)
-                    files = {'image': ("image.jpeg", stream, "image/jpeg")}
-                    response = requests.post(url, files=files, timeout=60)
-                    response.raise_for_status()
-
-                except RequestException as e:
-                    logger.warning("Could not send pic: {}".format(e))
-                logger.info("Pic sent!")
-
+            filename = datetime.now().strftimee("%m_%d_%Y__%H_%M_%S")
+            camera = PiCamera()
+            camera.resolution = (1024, 768)
+            camera.start_preview()
+            # Camera warm-up time
+            time.sleep(2)
+            camera.capture('/home/pi/photos/{}.jpg'.format(filename))
+            logger.info("Picture taken! Snap!")
             time_stamp = datetime.now()
-
-            # All done!
 
     pir.when_motion = motion_detected
 
